622597e12dfcb964d23dd6cf4313663fee37a163
github username <github email address>
2018-10-31 18:28:48 -0500

    square array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |element|
+    new_array << element ** 2
+  end
+    return new_array
 end
+
+#def square_array(array)
+#    new_array = []
+#    array.each do |array|
+#    new_array = array ** 2
+#    new_array << new_array
+#    return new_array
+#  end
+#end
